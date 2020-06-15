4fca82976d982743b8636413a8402f45867736d2
Min-kyu-Kim <kmk50009@naver.com>
2020-06-15 12:20:19 +0900

    Updates

---
+++ precache-manifest.3108638868219e72bde4a94979fab1ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f02e351e6f005995438f4af8ec4d7b1",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "0e30a9b0396cdf0e1356",
+    "url": "/movie_app_2020/static/css/main.4c7c9cf0.chunk.css"
+  },
+  {
+    "revision": "b17fb38856ffb36d44ef",
+    "url": "/movie_app_2020/static/js/2.14c54020.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.14c54020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e30a9b0396cdf0e1356",
+    "url": "/movie_app_2020/static/js/main.7e93a502.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
